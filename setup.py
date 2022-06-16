00970fbd642c064123539ce8cb1977539929479b
Yousef Mohamad Abdala <95507626+blackaries506@users.noreply.github.com>
2022-06-16 14:47:52 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+import os
+os.system("python3 -m pip install -r requirements.txt")
+os.system("sudo apt install figlet")
+
+print(""""
+    name='Fishing Tool',
+    version='1.0',
+    packages=['en'],
+    author='yousef mohamad abdala',
+    author_email='yousefmohamad506@gmail.com',
+    description='Hack Tool'""""")
